f2d3a81a3d9018c3091b16bfa70d1f8ebafeb9de
Ppllupold <kotnazar2005@gmail.com>
2025-05-16 19:02:17 +0300

    blacked files

--- theater_api/apps.py
+++ theater_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TheaterApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theater_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "theater_api"
